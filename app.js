f6cbaa0c9aadb012630f4b0c6f99c2c4f802789c
Tomgtech <tom.gendall@yahoo.com>
2024-01-11 15:04:10 +0000

    this is it

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+// let age = 17;
+// let movieRating = "PG";
+// if (age > 17) {
+//   console.log("you can watch this film");
+// } else if (age === 17) {
+//   if (movieRating === "PG") {
+//     console.log("you can watch the movie");
+//   }
+// }
+
+// let age = 17;
+// let movieRating = "12";
+// if (age > 17) {
+//   console.log("you can watch this film");
+// } else if (age === 17) {
+//   if (movieRating === "12") {
+//     console.log("you can watch the movie");
+//   }
+// }
+
+let age = 20;
+let movieRating = "18";
+
+if (age > 18) {
+  console.log("you can watch this movie");
+} else if (age < 18) {
+  if (movieRating === "18") {
+    console.log("you can't watch the movie");
+  }
+}
